f02832367d2948a463a2f67cd40451f61c25269f
EwarystLawecki <e.lawecki@tkhtechnology.com>
2023-01-17 22:38:44 +0100

    feat: improve ui

--- frontend/src/pages/DashboardPage/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage/DashboardPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route, Link } from "react-router-dom";
@@ -13,14 +14,14 @@ const NavBar = styled.div`
   position: fixed;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   width: 20vw;
-  background-color: red;
+  background-color: #1d549c;
   height: 100vh;
   left: 0;
   top: 0;
   padding: 1rem 3rem;
+  box-shadow: 14px 0px 36px 0px rgba(0, 0, 0, 0.75);
 `;
 
 const Content = styled.div`
@@ -29,6 +30,43 @@ const Content = styled.div`
   margin-left: 40vw;
   background-color: azure;
 `;
+
+const LinkWrapper = styled.div`
+  padding: 1rem 2rem;
+  margin: 0.5rem;
+  background-color: #a1c2e0;
+  width: 60%;
+  border-radius: 26px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #295479;
+    a {
+      color: #fff !important;
+    }
+  }
+
+  a {
+    color: #000;
+    text-decoration: none !important;
+  }
+
+  &:last-child {
+    margin-bottom: 3rem;
+  }
+`;
+
+const Logo = styled.div`
+  width: 100%;
+  font-size: 2rem;
+  font-weight: 900;
+  margin-bottom: 18vh;
+`;
+
+const SignOutButtonWrapper = styled.div`
+  margin-top: 8rem;
+`;
+
 interface Props {
   userType?: UserType;
 }
@@ -43,10 +81,23 @@ function DashboardPage({ userType }: Props) {
   return (
     <>
       <NavBar>
+        <Logo>E-DZIENNIK</Logo>
         {NavBarElementsByType.get(userType!)?.map((element) => (
-          <Link to={element.path}>{element.name}</Link>
+          <LinkWrapper>
+            <Link to={element.path}>{element.name}</Link>
+          </LinkWrapper>
         ))}
-        <button onClick={handleOnClick}>Wyloguj się</button>
+        <SignOutButtonWrapper>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={handleOnClick}
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Wyloguj się
+          </Button>
+        </SignOutButtonWrapper>
       </NavBar>
       <Content>
         <Routes>
